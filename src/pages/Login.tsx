1978c93a8f42a92bf6fd03c37cd18a92f7355acf
zermoser <kittipoj.na@gmail.com>
2024-06-11 15:20:35 +0700

    update swal to project

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,25 +2,24 @@ import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import axios from 'axios'; // Import Axios
+import Swal from 'sweetalert2';
 import { UserContext } from '../context/UserContext';
 
 interface LoginProps {
   onLogin: (username: string, password: string) => Promise<void>;
 }
 
-const Login: React.FC<LoginProps> = () => {
+const Login: React.FC<LoginProps> = ( ) => {
   const [username, setUsername] = useState('emilys');
   const [password, setPassword] = useState('emilyspass');
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
   const { dispatch } = useContext(UserContext);
   const navigate = useNavigate();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError(null);
     try {
       const response = await axios.post('https://dummyjson.com/auth/login', {
         username,
@@ -30,7 +29,11 @@ const Login: React.FC<LoginProps> = () => {
       dispatch({ type: 'SET_USER', payload: username });
       navigate('/home');
     } catch (err) {
-      setError('Invalid username or password. Please try again.');
+      Swal.fire({
+        icon: 'error',
+        title: 'Login Failed',
+        text: 'Invalid username or password. Please try again.',
+      });
     } finally {
       setLoading(false);
     }
@@ -54,7 +57,6 @@ const Login: React.FC<LoginProps> = () => {
         <div className="w-full lg:w-2/3 px-4 lg:px-8 py-8 lg:py-0">
           <form className="bg-white p-6 rounded-lg shadow-md" onSubmit={handleLogin}>
             <h2 className="text-2xl font-bold text-center text-blue-500 mb-6">My Social Media</h2>
-            {error && <div className="text-red-500 mb-4">{error}</div>}
             <input
               type="text"
               placeholder="Username"
